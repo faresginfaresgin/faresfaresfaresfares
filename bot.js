8840b4b6a09973a4cd85601fcf2f27f3848193d7
faresginfaresgin <44483555+faresginfaresgin@users.noreply.github.com>
2018-11-07 00:01:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,43 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '+'
-
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`invited by A.B.K `,"http://twitch.tv/Alpha Codes")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
+  client.user.setGame(':two_hearts: بسم اللة الرحمن الرحيم :two_hearts:','https://www.twitch.tv/TEST-Broadcast%27);
+  console.log('---------------');
   console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+  console.log('---------------')
 });
 
+client.login('NTA2OTIwODA2MTU3MjU0Njc4.DrpKWA.rjs4S3z4R3MVSEeNvK2PHPBrZ68');
 
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(:hearts:منور السيرفر:hearts:
+:crown:اسمك:crown: : ${member}  
+أنت العضو رقم: : ${member.guild.memberCount}) 
+}).catch(console.error)
+})
+const adminprefix = "^";
+const devs = ['465485850432765963','465485850432765963'];
+client.on('message', message => {
+  var argresult = message.content.split(`).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(${argresult} تم تغيير بلاينق البوت إلى )
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(${argresult} : تم تغيير أسم البوت إلى)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'avatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(${argresult} : تم تغير صورة البوت);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(تم تغيير تويتش البوت إلى  ${argresult}`)
+}
+});
